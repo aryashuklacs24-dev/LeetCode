423830ddeafdf4094d0bc27849de359b19b5713e
aryashuklacs24-dev <arya.shuklacs24@gmail.com>
2026-04-22 16:17:14 +0530

    Time: 9 ms (98.76%) | Memory: 59.5 MB (34.71%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode mid=middleNode(head);
+        ListNode headb=mid.next;
+        mid.next=null;
+        ListNode A=sortList(head);
+        ListNode B=sortList(headb);
+        return mergeTwoLists(A,B);
+        
+    }
+   public ListNode middleNode(ListNode head) {
+    ListNode s = head;
+    ListNode f = head.next;  
+
+    while (f != null && f.next != null) {
+        s = s.next;
+        f = f.next.next;
+    }
+    return s; 
+}
+
+     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode temp = dummy;
+        
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                dummy.next=l1;
+                dummy=dummy.next;
+                l1=l1.next;
+
+            }
+            else{
+                dummy.next=l2;
+                dummy=dummy.next;
+                l2=l2.next;
+
+            }
+        }
+        if(l1==null){
+            dummy.next=l2;
+        }
+        else{
+             dummy.next=l1;
+        }
+
+        return temp.next;
+        
+    }
+}
