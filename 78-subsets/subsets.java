2cc553a88285019e9e9abdfd6d5f9e4a3b0ab465
aryashuklacs24-dev <arya.shuklacs24@gmail.com>
2026-09-17 12:36:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (7.03%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,25 +1,19 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-       return  getPowerSet(nums);
+         List<List<Integer>> result = new ArrayList<>();
+        ArrayList<Integer>temp=new ArrayList<>();
+        // int[]arr={1,2,3};
+        fun(nums, 0, temp, result);
+        return result;
+        
     }
-     public List<List<Integer>> getPowerSet(int[] nums) {
-        int n = nums.length;
-
-        int subsets = 1 << n;
-
-        List<List<Integer>> ans = new ArrayList<>();
-
-        for (int num = 0; num < subsets; num++) {
-            List<Integer> subset = new ArrayList<>();
-
-            for (int i = 0; i < n; i++) {
-                if ((num & (1 << i)) != 0) {
-                    subset.add(nums[i]);
-                }
-            }
-
-            ans.add(subset);
+    public static void fun(int[]arr,int idx,List<Integer>temp,List<List<Integer>>res){
+        
+            res.add(new ArrayList<>(temp));
+        for(int i=idx;i<arr.length;i++){
+            temp.add(arr[i]);
+            fun(arr, i+1, temp, res);
+            temp.remove(temp.size()-1);
         }
-        return ans;
     }
 }
