e506e747aa7e9f940153ad7d30c6c24b45a58d7b
aryashuklacs24-dev <arya.shuklacs24@gmail.com>
2026-09-20 15:10:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (15.20%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,34 +1,23 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        rightDFS(root, 0, res);
-        return res;
+        List<Integer> ans = new ArrayList<>();
+        rightview(root, 0, ans);
+        return ans;
     }
 
-     public void rightDFS(TreeNode node, int level, List<Integer> res) {
-        if (node == null) return;
+    int md = -1;
 
-        if (res.size() == level)
-            res.add(node.val);
-        rightDFS(node.right, level + 1, res);
-        rightDFS(node.left, level + 1, res);
+    public void rightview(TreeNode root, int cl, List<Integer> ans) {
+        if (root == null) {
+            return;
+        }
+
+        if (md < cl) {
+            ans.add(root.val);
+            md = cl;
+        }
+
+        rightview(root.right, cl + 1, ans);
+        rightview(root.left, cl + 1, ans);
     }
-        
-    
-
 }
